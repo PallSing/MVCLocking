c06a5b026a994c8e5a33694b6bc114dfcf8aef6e
Singh <PallSing@ITVPLC.ADS>
2017-11-29 15:26:51 +0000

    Add project files.

---
+++ MVCLocking/Models/Area.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCLocking.Models
+{
+    public class Area
+    {
+        public int macroAreaID;
+        public string macroAreaCode;
+        public int parentMacroAreaID;
+        public string macroAreaName;
+    }
+}
